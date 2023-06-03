5da94332096be10e17cab3e1e67c95ea9b7fe138
Prachi Telharkar <telharkarprachi@gmail.com>
2023-06-03 19:18:05 +0530

    home

---
+++ src/Components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+const Home = () => {
+  return (
+    <>
+        <section className="home" id="home">
+            <div className="home-text">
+                <h1>FLORALHAVEN </h1>
+                <h3>Captivating Petals: Unveiling the <span>Artistry</span> and <span>Fragrance</span>
+                <br /> of Flowers</h3>
+                <p>Unleash Your Senses, Immerse in Floral Wonder</p>
+                <a href="/" className="btn">Shop Now</a>
+            </div>
+         
+        </section>
+        <div className="arrival">
+            <div className="arrival-text">
+            <h1>New Arrival</h1>
+            <p>Discover a world of botanical marvels with our new arrival flowers, each delicately<br /> crafted  to elevate your moments and create lasting memories</p>
+            <h3>View collection <i class="fas fa-arrow-right-long"></i>
+</h3>
+        </div>
+        </div>
+    </>
+  )
+}
+
+export default Home
